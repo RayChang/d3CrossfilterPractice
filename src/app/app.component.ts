04139db5683a605a51ccf302fdd72f4bc89906b6
Ray Chang <mofies@gmail.com>
2019-07-09 18:31:04 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DashboardService } from 'src/share/service/Dashboard.service';
-import { select, Selection } from 'd3';
+import {
+  select, Selection,
+  scaleLinear, ScaleLinear,
+  axisLeft, axisBottom, Axis
+} from 'd3';
 
 @Component({
   selector: 'app-root',
@@ -9,26 +13,64 @@ import { select, Selection } from 'd3';
 })
 export class AppComponent implements OnInit {
   @ViewChild('weightHeightChart') weightHeightChart: ElementRef;
-  chartContainer: Selection<SVGElement, {}, HTMLElement, any>;
+  chartContainer: Selection<HTMLElement, {}, HTMLElement, any>;
 
   private chartWidth = 960;
   private chartHeight = 300;
-  private margin = 50;
+  private margin = 40;
   private chartHeightWithoutMargin = this.chartHeight - this.margin;
+  private chart: Selection<SVGElement, {}, HTMLElement, any>;
+  private countScale: ScaleLinear<number, number>;
+  private yearScale: ScaleLinear<number, number>;
 
   constructor(
     private dashboardService: DashboardService
   ) {}
+
   ngOnInit() {
-    this.dashboardService.getData();
-    this.createSvg();
-  }
-  createSvg() {
     this.chartContainer = select(this.weightHeightChart.nativeElement);
-    this.chartContainer
+    this.dashboardService.getData()
+    .then(() => {
+      this.createSvg();
+      this.initScales();
+      this.drawAxes();
+    });
+  }
+
+  createSvg() {
+    this.chart = this.chartContainer
       .append<SVGElement>('svg')
       .attr('width', this.chartWidth)
       .attr('height', this.chartHeight);
-    console.log(this.chartContainer);
+  }
+
+  initScales() {
+    const maxCount = this.dashboardService.numberOfAthletesPerYearGroup.top(1)[0];
+    const chartWidth = this.chartWidth - this.margin;
+    const chartHeight = this.chartHeight - this.margin;
+
+    this.countScale = scaleLinear<number, number>().domain([0, maxCount.value.count]).range([chartHeight, this.margin]);
+    this.yearScale = scaleLinear<number, number>().domain([1890, 2020]).range([this.margin, chartWidth]);
+  }
+
+  drawAxes() {
+    const countAxis: Axis<number> = axisLeft<number>(this.countScale);
+    const yearAxis: Axis<number> = axisBottom<number>(this.yearScale);
+
+    this.chart
+      .append('g')
+      .attr('class', 'c-axis')
+      .attr('transform', `translate(${this.margin}, 0)`)
+      .call(countAxis);
+
+    this.chart
+      .append('g')
+      .attr('class', 'c-axis')
+      .attr('transform', `translate(0, ${this.chartHeightWithoutMargin})`)
+      .call(yearAxis);
+  }
+
+  drawLine() {
+
   }
 }
--- src/share/service/Dashboard.service.ts
+++ src/share/service/Dashboard.service.ts
@@ -4,41 +4,71 @@ import { dsv } from 'd3';
 import { Crossfilter, Dimension, Group } from 'crossfilter2';
 import * as crossfilter from 'crossfilter2';
 
+class AthletesPerYearGroup {
+  year: number = null;
+  season: string = null;
+  count = 0;
+  valueOf() {
+    return this.count;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
   private ndx: Crossfilter<Athletes>;
   athletesDimension: Dimension<Athletes, string>;
-  numberOfAthletesPerYearGroup: Group<Athletes, string, string>;
+  numberOfAthletesPerYearGroup: Group<Athletes, AthletesPerYearGroup, AthletesPerYearGroup>;
 
   constructor() { }
 
   private createGroupFromDimension() {
-    this.numberOfAthletesPerYearGroup = this.athletesDimension.group();
-    console.log(this.numberOfAthletesPerYearGroup.all());
+    this.numberOfAthletesPerYearGroup = this.athletesDimension.group<AthletesPerYearGroup, AthletesPerYearGroup>()
+      .reduce(
+        // reduceAdd()
+        (output, input) => {
+          ++output.count;
+          output.year = input.year;
+          output.season = input.season;
+          return output;
+        },
+        // reduceRemove()
+        (output, input) => {
+          --output.count;
+          output.year = input.year;
+          output.season = input.season;
+          return output;
+        },
+        // reduceInitial()
+        () => new AthletesPerYearGroup()
+      );
   }
 
   getData() {
-    dsv(',', '/assets/athlete_events.csv', row => ({
-      age: row['Age'] !== 'NA' ? +row['Age'] : null,
-      city: row['City'],
-      event: row['Event'],
-      games: row['Games'],
-      height: row['Height'] !== 'NA' ? +row['Height'] : null,
-      weight: row['Weight'] !== 'NA' ? +row['Weight'] : null,
-      medal: row['Medal'],
-      name: row['Name'],
-      season: row['Season'],
-      sex: row['Sex'],
-      sport: row['Sport'],
-      team: row['Team'],
-      year: row['Year'] !== 'NA' ? +row['Year'] : null
-    })).then(result => {
-      console.log(result.splice(0, 10));
-      this.ndx = crossfilter(result);
-      this.athletesDimension = this.ndx.dimension(d => d.games);
-      this.createGroupFromDimension();
+    return new Promise(resolve => {
+      dsv(',', '/assets/athlete_events.csv', row => ({
+        age: row['Age'] !== 'NA' ? +row['Age'] : null,
+        city: row['City'],
+        event: row['Event'],
+        games: row['Games'],
+        height: row['Height'] !== 'NA' ? +row['Height'] : null,
+        weight: row['Weight'] !== 'NA' ? +row['Weight'] : null,
+        medal: row['Medal'],
+        name: row['Name'],
+        season: row['Season'],
+        sex: row['Sex'],
+        sport: row['Sport'],
+        team: row['Team'],
+        year: row['Year'] !== 'NA' ? +row['Year'] : null
+      })).then(result => {
+        // 將取回的結果轉換成 crossfilter 物件
+        this.ndx = crossfilter(result);
+        // 用 ndx 中的 games 建立維度
+        this.athletesDimension = this.ndx.dimension(d => d.games);
+        this.createGroupFromDimension();
+        resolve();
+      });
     });
   }
 
